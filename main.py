a1439f2ea63f5265e97a93095ebd138994bc288e
MustafaTheCoder <73116655+MustafaTheCoder@users.noreply.github.com>
2021-09-19 16:09:07 +0500

    Main

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-import psycopg2
+print("""
+========== Password Manager ============
+[1] View
+[2] Add
+[3] Quit
+========================================
+""")
 
-connection = psycopg2.connect(user = "postgres",
-password = "singer2005*",
-host = "127.0.0.1",
-port = "5432",
-database = "Test")
 
-cursor = connection.cursor()
-cursor.execute("Select * From products")
-cursor.fetchall()
+
+while True:
+    user_inp = int(input("Option: "))
+
+    if user_inp == 1:
+        with open("data.txt", "r") as f:
+            data = f.read()
+        print(data)
+
+    if user_inp == 2:
+        add = input("Enter Password: ")
+        add = str(add)
+
+        with open("data.txt", "w") as f:
+            f.write(add)
+
+        print("Password Saved!")
+
+    if user_inp == 3:
+        break    
